cef21542679ba3138753e72058ad2ede2e2ef42b
Max <maxstouten@hotmail.com>
2021-01-20 18:12:44 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h3>
+      hello
+    </h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
