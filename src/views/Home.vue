60fb313c40bc765de03b912d2f7236a856ac1f27
Max <maxstouten@hotmail.com>
2021-01-21 10:37:41 +0100

    added logic for pokemons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <h3>
-      hello
-    </h3>
+  <div class="w-full flex justify-center">
+    <input placeholder="Enter Pokemon here" type="text"
+           class="mt-10 p-2 border-blue-500 border-2" v-model="text" />
+
+  </div>
+
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+
+
+    <div
+            class="ml-4 text-2xl text-blue-400"
+            v-for="(pokemon, idx) in filteredPokemon"
+            :key="idx"
+    >
+      <router-link :to="`/about/${urlIdLookup[pokemon.name]}`">
+        {{ pokemon.name }}
+      </router-link>
+    </div>
+    <div
+            class="ml-4 text-2xl text-blue-400"
+            v-for="(pokemon, idx) in pokemons"
+            :key="idx"
+    >
+      <router-link :to="`/about/${urlIdLookup[pokemon.name]}`">
+        {{ pokemon.name }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+  import { reactive, toRefs, computed } from "vue";
+  export default {
+    setup() {
+      const state = reactive({
+        pokemons: [],
+        filteredPokemon: computed(()=> updatePokemon()),
+        text: "",
+        urlIdLookup: {}
 
-export default {
-  name: 'Home',
-  components: {
+      });
+      fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+              .then((res) => res.json())
+              .then((data) => {
+                console.log(data.results[0]);
+                state.pokemons = data.results;
+                state.urlIdLookup = data.results.reduce((acc, cur, idx)=>
+                                acc = {...acc, [cur.name]:idx+1 }
+                        ,{})
+                console.log('url',state.urlIdLookup+1)
 
-  }
-}
+              });
+      function updatePokemon(){
+        if(!state.text){
+          return []
+        }
+        return state.pokemons.filter((pokemon)=>
+                pokemon.name.includes(state.text)
+        )
+      }
+
+      return { ...toRefs(state) };
+    }
+  };
 </script>
